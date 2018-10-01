269342b5c722b9ba8ce4363bb19d1b7733a807d3
victorpinon <victorpinonrattia@hotmail.com>
2018-10-01 20:50:29 +0200

    Added Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,61 @@
+import java.util.Date;
+
+public class Employee {
+    public String name;
+    public String surname;
+    public Date birthday;
+    public double salary;
+    public String companyName;
+
+    public Employee(String name, String surname, Date birthday, double salary, String companyName) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.salary = salary;
+        this.companyName = companyName;
+    }
+
+    public Employee(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
