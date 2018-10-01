92d640b59e42f1a763cc7f68adc434bba5a20f49
mbasart <mbd2797@gmail.com>
2018-10-01 19:50:48 +0200

    he canviat la classe companyManager

--- src/main/java/CompanyManager.java
+++ src/main/java/CompanyManager.java
@@ -1,2 +1,11 @@
+import java.util.Date;
+import java.util.List;
+
 public interface CompanyManager {
+    public void addCompany(String companyName, String description);
+    public void addEmployee(String name, String surname, Date birthday, double salary, String companyName) throws MyException;
+    public List<Employee> findAllEmployeesOrderedByName();
+    public List<Employee> findAllEmployeesOrderedBySalary();
+    public List<Employee> employees(String company);
+    public List<Company> findAllCompanies();
 }
